0e42135a3d9df6e4eaff6032123452ae81c9b263
Gabriel Barbieri <107634249+Gabrieluser430@users.noreply.github.com>
2023-02-21 21:03:15 -0300

    Update About.js

--- components/About.js
+++ components/About.js
@@ -5,7 +5,7 @@ export default function About() {
         <div className="card-div">
             <h2 className="card-title">Sobre</h2>
             <p className="card-text">
-            Olá, sou um aspirante a desenvolvedor de front-end apaixonado por aprender e manter-se atualizado com as últimas evoluções tecnológicas. Estou ativamente procurando um estágio para ganhar experiência prática e aprimorar minhas habilidades na área. Adoro aprender e sempre estou ansioso por ficar atualizado com as melhores práticas e medidas de segurança no desenvolvimento de front-end.
+            Sou uma pessoa dedicada e apaixonada por T.I, especialmente programação. Comecei a estudar programação com 17 anos e desde então, tenho trabalhado arduamente para adquirir uma variedade de habilidades técnicas. Atualmente estou estudando e fazendo projetos com React.
             </p>
         </div>
     )
